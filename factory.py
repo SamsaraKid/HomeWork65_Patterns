ab2a74fbee8a3755c4437a6ea49feae48a62d72a
korolevaj <koroliev@yandex.ru>
2023-11-03 13:05:34 +0300

    завод

--- factory.py
+++ factory.py
@@ -16,17 +16,21 @@ class Functions(ABC):
 
 
 class Car(Functions):
-    def __init__(self, rashod, volume):
+    def __init__(self, rashod, volume, speed):
         self._rashod = rashod  # расход топлива, л/100км
         self._volume = volume  # объём топливного бака, л
+        self._speed = speed     # скорость км/ч
         self.__fuel = 0        # количество топлива в баке, л
 
-    def ride(self, distance):
+    def ride(self, time):
         rashod_per_km = self._rashod / 100
+        distance = self._speed * time
         if self.__fuel >= rashod_per_km * distance:
             print(f'\tПроехали {distance}км, потратили {rashod_per_km * distance}л топлива')
+            self.__fuel -= rashod_per_km * distance
         elif self.__fuel > 0:
             print(f'\tПроехали {self.__fuel / rashod_per_km}км, потратили {self.__fuel}л топлива. Топлива больше нет')
+            self.__fuel = 0
         else:
             print('\tНет топлива')
 
@@ -41,19 +45,20 @@ class Car(Functions):
 
 class Pickup(Car):
     def __init__(self):
-        super().__init__(20, 85)
+        super().__init__(20, 85, 90)
 
 
 class Citycar(Car):
     def __init__(self):
-        super().__init__(10, 45)
+        super().__init__(10, 45, 110)
 
 
 class UnknownCarException(Exception):
     def __str__(self):
         return '\tНеизвестный тип автомобиля:'
 
-class Factory():
+
+class Factory:
     def create(self, name):
         try:
             if name == 'pickup':
@@ -68,6 +73,21 @@ class Factory():
             print(e, name)
 
 
+class AbsImprover(Functions):
+    def __init__(self, object):
+        self.object = object
+
+    def ride(self, distance):
+        self.object.ride()
+
+    def fill(self):
+        self.object.fill()
+
+    def fueldat(self):
+        self.object.fueldat()
+
+
+
 carfactory = Factory()
 print('Производим пикап')
 car1 = carfactory.create('pickup')
@@ -77,14 +97,18 @@ print('Производим мотоцикл')
 car3 = carfactory.create('moto')
 
 print('Катаемся на пикапе')
-car1.ride(10)
+car1.ride(2)
 car1.fill()
 car1.fueldat()
-car1.ride(500)
+car1.ride(2)
+car1.fueldat()
 
 print('Катаемся на городском авто')
-car2.ride(10)
+car2.ride(2)
 car2.fill()
 car2.fueldat()
-car2.ride(500)
+car2.ride(2)
+car1.fueldat()
+
+
 
