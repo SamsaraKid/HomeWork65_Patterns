85ddfe4a7604b492010c1c44363af2caa0d3b00f
koany <koroliev@yandex.ru>
2023-11-05 18:37:47 +0300

    сделал декораторы и начал наблюдателя

--- factory.py
+++ factory.py
@@ -15,32 +15,38 @@ class Functions(ABC):
         pass
 
 
+
+def ridefunction(fuel, rashod_per_km, distance):
+    if fuel >= rashod_per_km * distance:
+        print(f'\tПроехали {distance}км, потратили {rashod_per_km * distance}л топлива')
+        fuel -= rashod_per_km * distance
+    elif fuel > 0:
+        print(f'\tПроехали {fuel / rashod_per_km}км, потратили {fuel}л топлива. Топлива больше нет')
+        fuel = 0
+    else:
+        print('\tНет топлива')
+    return fuel
+
+
 class Car(Functions):
     def __init__(self, rashod, volume, speed):
-        self._rashod = rashod  # расход топлива, л/100км
-        self._volume = volume  # объём топливного бака, л
-        self._speed = speed     # скорость км/ч
-        self.__fuel = 0        # количество топлива в баке, л
+        self.rashod = rashod  # расход топлива, л/100км
+        self.volume = volume  # объём топливного бака, л
+        self.speed = speed     # скорость км/ч
+        self.fuel = 0        # количество топлива в баке, л
 
     def ride(self, time):
-        rashod_per_km = self._rashod / 100
-        distance = self._speed * time
-        if self.__fuel >= rashod_per_km * distance:
-            print(f'\tПроехали {distance}км, потратили {rashod_per_km * distance}л топлива')
-            self.__fuel -= rashod_per_km * distance
-        elif self.__fuel > 0:
-            print(f'\tПроехали {self.__fuel / rashod_per_km}км, потратили {self.__fuel}л топлива. Топлива больше нет')
-            self.__fuel = 0
-        else:
-            print('\tНет топлива')
+        rashod_per_km = self.rashod / 100
+        distance = self.speed * time
+        self.fuel = ridefunction(self.fuel, rashod_per_km, distance)
 
     def fill(self):
-        print(f'\tЗалили в бак {self._volume - self.__fuel}л топлива')
-        self.__fuel = self._volume
+        print(f'\tЗалили в бак {self.volume - self.fuel}л топлива')
+        self.fuel = self.volume
 
     def fueldat(self):
-        print(f'\tВ баке {self.__fuel}л топлива')
-        return self.__fuel
+        print(f'\tВ баке {self.fuel}л топлива')
+        return self.fuel
 
 
 class Pickup(Car):
@@ -65,7 +71,7 @@ class Factory:
                 print('\tЗавод выпустил пикап')
                 return Pickup()
             elif name == 'citycar':
-                print('\tЗавод выпустил горолское авто')
+                print('\tЗавод выпустил городское авто')
                 return Citycar()
             else:
                 raise UnknownCarException
@@ -77,7 +83,7 @@ class AbsImprover(Functions):
     def __init__(self, object):
         self.object = object
 
-    def ride(self, distance):
+    def ride(self, time):
         self.object.ride()
 
     def fill(self):
@@ -89,42 +95,73 @@ class AbsImprover(Functions):
 
 class Nitro(AbsImprover):
     def ride(self, time):
-        rashod_per_km = self._rashod / 100
-        distance = self._speed * time
-        if self.__fuel >= rashod_per_km * distance:
-            print(f'\tПроехали {distance}км, потратили {rashod_per_km * distance}л топлива')
-            self.__fuel -= rashod_per_km * distance
-        elif self.__fuel > 0:
-            print(f'\tПроехали {self.__fuel / rashod_per_km}км, потратили {self.__fuel}л топлива. Топлива больше нет')
-            self.__fuel = 0
-        else:
-            print('\tНет топлива')
+        rashod_per_km = 2 * self.object.rashod / 100
+        distance = 2 * self.object.speed * time
+        self.object.fuel = ridefunction(self.object.fuel, rashod_per_km, distance)
 
 
+class Econom(AbsImprover):
+    def ride(self, time):
+        rashod_per_km = self.object.rashod / 100 / 2
+        distance = self.object.speed * time / 2
+        self.object.fuel = ridefunction(self.object.fuel, rashod_per_km, distance)
+
+
+class NeedForSpeed():
+    def __init__(self, time):
+        self.riders = set()
+        self.time = time
+
+    def enter(self, rider):
+        self.riders.add(rider)
+
+    def exit(self, rider):
+        self.riders.remove(rider)
+
+    def go(self):
+        print('\tГонка стартовала')
+        for num, car in enumerate(self.riders, start=1):
+            if isinstance(car, Car):
+                print('Участник номер', num)
+                car.ride(self.time)
 
 carfactory = Factory()
-print('Производим пикап')
-car1 = carfactory.create('pickup')
-print('Производим городское авто')
+print('Производим пикапы')
+pick1 = carfactory.create('pickup')
+pick2 = carfactory.create('pickup')
+pick3 = carfactory.create('pickup')
+print('Производим городские авто')
+car1 = carfactory.create('citycar')
 car2 = carfactory.create('citycar')
+car3 = carfactory.create('citycar')
 print('Производим мотоцикл')
-car3 = carfactory.create('moto')
+car4 = carfactory.create('moto')
 
 print('Катаемся на пикапе')
-car1.ride(2)
-car1.fill()
-car1.fueldat()
-car1.ride(2)
-car1.fueldat()
+pick1.ride(2)
+pick1.fill()
+pick1.fueldat()
+pick1.ride(2)
+pick1.fueldat()
 
 print('Катаемся на городском авто')
 car2.ride(2)
 car2.fill()
 car2.fueldat()
 car2.ride(2)
-car1.fueldat()
+car2.fueldat()
 
+
+print('Машина с нитро')
+car2.fill()
 car2 = Nitro(car2)
-car2.ride(2)
-car1.fueldat()
+car2.ride(1)
+car2.fueldat()
+
+print('Машина эконом')
+car3.fill()
+car3 = Econom(car3)
+car3.ride(1)
+car3.fueldat()
+
 
